5764544e304521c0337aab5e018b15323f5c312b
María Laura Escanes <129324078+EscanesMLaura@users.noreply.github.com>
2024-04-28 17:57:42 -0300

    main

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -32,7 +32,7 @@ export const Inicio = () => {
                     <a href="mailto:escanesmarialaura@gmail.com?subject=Correo enviado desde tu Portafolio&body=Me interesa contactarme con vos" target="_blank" rel="noreferrer">
                         <button className="buttonContactame">Contactame</button>
                     </a>
-                    <a href="../Escanes María Laura - CV.pdf" target="_blank" rel="noreferrer">
+                    <a href="../CV - Escanes María Laura.pdf" target="_blank" rel="noreferrer">
                         <button className="buttonDownload">Descargar CV</button>
                     </a>
                 </div>
